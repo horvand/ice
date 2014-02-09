2df6b0726b280fa8b25423c74be23b9d1d065365
Edward Tate <edward.tate@erlang-solutions.com>
2014-02-09 12:10:36 +0000

    Added extremely trivial benchmarking function for convenience.

---
+++ src/ice_bench.erl
@@ -0,0 +1,10 @@
+-module(ice_bench).
+
+-export([time_f/1]).
+
+time_f(Filename) ->
+  T1 = erlang:now(),
+  ice:f(Filename),
+  T2 = erlang:now(),
+  io:format("Time taken: ~p~n", [timer:now_diff(T2, T1) / 1000000]).
+  
